43c84adc6cc3e65774587a788090142d972c25f7
vaughnpaul <vaughn@c2minteractive.com>
2012-01-29 14:32:57 -0500

    add includes

---
+++ c2m/includes/contact-callout.php
@@ -0,0 +1,28 @@
+													  <div><img src="/index_images/phonenumber.gif" alt="" width="245" height="45" /></div>
+													  <h5 class="callout">Consultations are always free and informative! Call, use the button to the right to book an appointment, or click below to online chat now!</h5>
+<div><img src="/index_images/freeconsultation.gif" alt="c2m interactive free consultation" /></div>
+                                                        <div class="callout">
+                                                          <h5 class="text"><strong>QUESTIONS? IDEAS?</strong><br />
+                                                          Chat online live with specialist standing by to assist you</h5>                                                       </div>
+                                                        <!-- start chat widget -->
+                                                      <div><a href="" target="_blank" onClick="window.open(encodeURI('https://oggchat.icoa.com/cp?font=Verdana&fontsize=10pt&bgcolor=FFFFFF&cid=c094a3a22ba214c1012bad9f3f240430&website=c2minteractive.com&text=_oc-c&height=450&width=380&r='+document.referrer), 'OggChat', 'toolbar=0,scrollbars=1,location=0,status=0,menubar=0,resizable=1,width=380,height=450');return false;"><img src="http://oggchat.icoa.com/cb?cid=c094a3a22ba214c1012bad9f3f240430&link=_oc-c" border="0"></a>
+<!-- OggChat Monitor -->
+<script type="text/javascript">//<![CDATA[
+var page = {
+'host':'oggchat.icoa.com',
+'cid':'c094a3a22ba214c1012bad9f3f240430',
+'act':'c094a3a22ba214c1012bad9f3e92042f',
+'font':'Verdana',
+'fs':'10pt',
+'p':'120'
+};
+function mon(){
+var base = (("https:" == document.location.protocol) ? "https://oggchat.icoa.com" : "http://oggchat.icoa.com");
+var url = base+"/js/monitor.js";
+var e = document.createElement('script');e.src = url;e.type ='text/javascript';document.getElementsByTagName('head')[0].appendChild(e);}if (window.addEventListener){window.addEventListener('load',mon, false);}else if(window.attachEvent){window.attachEvent('onload',mon);}
+//]]></script>
+<!-- End OggChat Monitor -->
+<!-- End OggChat Monitor --></div>
+                                                        <!-- End OggChat Monitor -->
+                                                        <!-- end chat widget -->
+                                                      <div><a href="/contact.php"><img  border="0" src="/index_images/leavemessage.gif" alt="Contact Us" /></a></div>
