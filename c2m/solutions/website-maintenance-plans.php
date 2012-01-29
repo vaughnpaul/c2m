a6cdb778cadb33e78503ca1f5bf9b6bbf2f02cbe
vaughnpaul <vaughn@c2minteractive.com>
2012-01-29 14:13:44 -0500

    add pland

---
+++ c2m/solutions/website-maintenance-plans.php
@@ -0,0 +1,355 @@
+<?php include("$_SERVER[DOCUMENT_ROOT]/global-inc.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title><?php echo $mainTitle; ?> - Web Design, Web Development and Strategic Consulting</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="../style.css" rel="stylesheet" type="text/css" />
+<link href="../layout.css" rel="stylesheet" type="text/css" />
+
+		<script type="text/javascript" src="../swfobject.js"></script>
+		<script type="text/javascript">
+			var flashvars = {};
+			var params = {};
+			params.scale = "noscale";
+			params.salign = "tl";
+			params.wmode = "transparent";
+			params.allowfullscreen = "true";
+			var attributes = {};
+			swfobject.embedSWF("PhotoRotatorFX.swf", "PhotoRotatorFXDiv", "450", "300", "10.0.0", false, flashvars, params, attributes);
+		</script>
+<style>
+.ven{
+	margin:0px 0px 15px 70px;
+}
+</style>
+<script src="../maxheight.js" type="text/javascript"></script>
+    <!--[if IE]>
+    <script type="text/javascript" src="ie_png.js"></script>
+       <script type="text/javascript">
+           DD_belatedPNG.fix('.png');
+       </script>
+    <![endif]-->
+</script>
+
+
+<!-- TABLE ROW SCRIPT -->
+
+	<script type="text/javascript" src="js/jquery_002.js"></script>
+	<script type="text/javascript" src="js/functions.js"></script>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/packages.js"></script>
+	<script type="text/javascript" src="js/superfish.js"></script>
+	
+
+	<link href="js/960_24_col.css" rel="stylesheet" type="text/css" media="screen">
+	<link href="js/superfish.css" rel="stylesheet" type="text/css" media="screen">
+	<link href="js/template.css" rel="stylesheet" type="text/css" media="screen">
+
+
+
+<!-- END OF TABLE ROW -->
+	
+</head>
+<body id="page-6" onload="new ElementMaxHeight();">
+	<div class="top-tail"></div>
+    <div class="bottom-tail">
+        <div id="main">
+        <!--header -->
+          <div id="header">
+                <div class="wrapper">
+
+		
+                    <div class="logo float-l"><a href="/"><img alt="" src='/images/logo.gif' /></a></div>
+                    <div class="float-l">
+                        <ul class="wrapper">
+                            <li><a href="/"><img src="../images/m1-act.gif" alt="" /></a></li>
+                            <li><a href="<?php echo $profileLink; ?>"><img src="../images/page-1-m2.gif" alt="" /></a></li>
+                            <li><a href="<?php echo $portfolioLink; ?>"><img src="../images/m3.gif" alt="" /></a></li>
+                            <li><a href="<?php echo $solutionsLink; ?>"><img src="../images/m4.gif" alt="" /></a></li>
+                            <li><a href="<?php echo $contactLink; ?>"><img src="../images/m5.gif" alt="" /></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <img class="slogan" alt="" src='../images/slogan.jpg' />
+            </div>
+        <!--header end-->
+            <div id="content">
+            	<div class="row-1">                
+                    <div class="box-left">
+                        <div class="box-right">
+                            <div class="box-top-left">
+                                <div class="box-top-right">
+                                    <div class="box-bot-left">
+                                        <div class="box-bot-right">
+                                            <h2><a href="/">Home</a> <?php include("$_SERVER[DOCUMENT_ROOT]/addThis.php"); ?></h2>
+                                          <div class="inner">
+                                            <div class="wrapper">
+                                              <div class="col-1">
+                                                <h1 class="title1">Website Maintenance</h1>
+												
+												<p>
+													We provide professional website maintenance, web page maintenance, website management, e-commerce website maintenance and website analytics reviews.  We love serving our local North Jersey business community, but we can service any customer worldwide
+												</p>
+												<h2>Uses for Website Maintenance Plans</h2>	
+												<p>Your website maintenance plan can be used for many purposes.  Here are some examples: </p> 
+                  <div class="wrapper">
+                    	<ul class="float-l" style="margin-left:17%;">
+                            <li><a href="#">Advice and consultation</a></li>
+                            <li><a href="#">Creative direction</a></li>
+                            <li><a href="#">Graphic design</a></li>
+
+                            <li><a href="#">Content updates</a></li>
+                        </ul>
+                        <ul class="float-r" style="margin-right:18%;">
+                            <li><a href="#">Malesuada atnequeivamus</a></li>
+                            <li><a href="#">Eget nibhtiam cursuseovel</a></li>
+                            <li><a href="#">Nulla facilisieneane</a></li>
+                            <li><a href="#">Vestibulum ante ipsumrimis</a></li>
+
+                  </ul>
+                 </div>  
+												<p>
+													Keep your website updated with fresh accurate content.  This will not only make your visitors happy, but you will also improve your search engine rankings, attract more visitors and strengthen your online presence.
+												</p>
+													 
+												<p>
+													We offer subscription plans or pay-as-you-go. We have affordable website maintenance plans for those who sporadically update their website or those who regularly do.  We have a Pay-as-you-go plan for those who only need occasional updates, and recurring plans for those who need regular service
+												</p>
+													 
+												<p>
+													If you find that one of our web maintenance plans don't fit your needs, we are flexible.  Please call us at 201-340-6340 to discuss a custom website maintenance plan for you.
+												</p>
+													 
+												<p>
+													Consult the chart below for a description of our services
+												</p>
+												
+												
+												<table class="packages"><tbody>
+
+<tr>
+
+	<th class="chart-header-bg">
+		Feature
+	</th>
+	<th class="chart-header-bg" width="90">Pay as you go</th>
+	<th class="chart-header-bg" width="90"><img src="../images/2hours.gif" alt="Available" /></th>
+	<th class="chart-header-bg" width="90"><img src="../images/5hours.gif" alt="Available" /></th>
+	<th class="chart-header-bg" width="90"><img src="../images/8hours.gif" alt="Available" /></th>
+	<th class="chart-header-bg" width="90"><img src="../images/12hours.gif" alt="Available" /></th>
+</tr>
+	
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+	
+	<td class="aleft"><b><i title="send all support email requests to support@c2minteractive.com" class="ttip">Email Support</i></b></td>
+	
+	<td>--</td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b><i title="Our knowledgeable staff can give you advice about strategy and trends" class="ttip">Advice and Support</i></b></td>
+	
+	<td>$60/hr</td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b><i title="Our customers get access to our online helpdesk ticket system to help manage their support requests" class="ttip">Online Customer Support System</i></b></td>
+	
+	<td>included</td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b><i title="Servers sometimes go down. This service makes sure your website is up and available and will send out alerts if there is a problem" class="ttip">Site Monitoring</i></b></td>
+	
+	<td>--</td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b><i title="We maintain a backup copy of your website" class="ttip">Site Backup</i></b></td>
+	
+	<td>--</td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b><i title="Monthly customized analytics report sent to your email address" class="ttip">Custom Site Analytics Report</i></b></td>
+	
+	<td>$75 Setup</td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b><i title="We will take a look at your statistics and alert you to any opportunities or problems" class="ttip">Periodic Analytics Review*</i></b></td>
+	
+	<td>$50 Each</td>
+	
+	<td>--</td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b><i title="Analyze your traffic, and clicks and do comparison testing on conversion clicks" class="ttip">Advanced Analytics Review/Optimization*</i></b></td>
+	
+	<td>--</td>
+	
+	<td>--</td>
+	
+	<td>--</td>
+	
+	<td>--</td>
+	
+	<td><img src="../images/check.png" alt="Available" /></td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b class="padded">Turnaround time</b></td>
+	
+	<td>5 Business Days</td>
+	
+	<td>5 Business Days</td>
+	
+	<td>3 Business Days</td>
+	
+	<td>3 Business Days</td>
+	
+	<td>2 Business Days</td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b class="padded">Hours of Support</b></td>
+	
+	<td>--</td>
+	
+	<td>2</td>
+	
+	<td>5</td>
+	
+	<td>8</td>
+	
+	<td>12</td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b class="padded"></b></td>
+	
+	<td>--</td>
+	
+	<td>55</td>
+	
+	<td>50</td>
+	
+	<td>45</td>
+	
+	<td>40</td>
+	
+</tr>
+
+<tr style="background: none repeat scroll 0% 0% rgb(249, 249, 249);">
+
+	<td class="aleft"><b class="padded">Investment</b></td>
+	
+	<td>$60/hr ($20 min)</td>
+	
+	<td>$110</td>
+	
+	<td>$250</td>
+	
+	<td>$360</td>
+	
+	<td>$480</td>
+	
+</tr>
+
+
+	
+	 </tbody></table>
+												<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+				<div class="row-2">                
+                    <img alt="" src='../images/box3-bot.jpg' />
+                </div>
+            </div>  	        
+        </div>
+        <!--footer -->
+<?php include("$_SERVER[DOCUMENT_ROOT]/footer.php"); ?>
